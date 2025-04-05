ada01e67e18a5cd4629d01e5c996683349d6a7b5
GayatrixAnand <anandsharma788778@gmail.com>
2025-04-05 23:00:17 +0530

    Update userbot.py

--- VIKRANT/core/userbot.py
+++ VIKRANT/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("SYNTAX_WORLD")
-                await self.one.join_chat("Guppppp_Shuppppp")
+                await self.one.join_chat("New_sanatani")
+                await self.one.join_chat("Unique_society")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("SYNTAX_WORLD")
-                await self.one.join_chat("Guppppp_Shuppppp")
+                await self.two.join_chat("New_sanatani")
+                await self.one.join_chat("Unique_society")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("SYNTAX_WORLD")
-                await self.one.join_chat("Guppppp_Shuppppp")
+                await self.three.join_chat("New_sanatani")
+                await self.one.join_chat("Unique_society")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("SYNTAX_WORLD")
-                await self.one.join_chat("Guppppp_Shuppppp")
+                await self.four.join_chat("New_sanatani")
+                await self.one.join_chat("Unique_society")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("SYNTAX_WORLD")
-                await self.one.join_chat("Guppppp_Shuppppp")
+                await self.five.join_chat("New_sanatani")
+                await self.one.join_chat("Unique_society")
             except:
                 pass
             assistants.append(5)
